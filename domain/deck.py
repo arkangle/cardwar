0f39774c43774aca338d211294b28a29cb64fab3
Michael McHolm <mcholmm@gmail.com>
2014-12-30 22:55:27 -0500

    generate full deck and shuffle

--- domain/deck.py
+++ domain/deck.py
@@ -1,8 +1,22 @@
+from domain.card import Card
+import random
+
 class Deck:
+    @staticmethod
+    def generate(count=11,each=4):
+        deck = Deck()
+        for c in range(1,count+1):
+            for e in range(1,each+1):
+                deck.addCard(Card(c))
+        return deck
     def __init__(self):
         self.cards = []
     def addCard(self,card):
         self.cards.append(card)
     def getCards(self):
         return self.cards
+    def nextCard(self):
+        return self.cards.pop(0)
+    def shuffle(self):
+        random.suffle(self.cards)
 
--- test/test_domain/test_deck.py
+++ test/test_domain/test_deck.py
@@ -14,3 +14,26 @@ class TestDeck(unittest.TestCase):
         deck.addCard(Card(2))
         deck.addCard(Card(3))
         self.assertEqual(deck.getCards(),[Card(1),Card(2),Card(3)])
+    def testNextCard(self):
+        deck = Deck()
+        cards = deck.getCards()
+        deck.addCard(Card(1))
+        deck.addCard(Card(2))
+        deck.addCard(Card(3))
+        self.assertEqual(len(cards),3)
+        self.assertEqual(deck.nextCard(),Card(1))
+        self.assertEqual(len(cards),2)
+        self.assertEqual(deck.nextCard(),Card(2))
+        self.assertEqual(len(cards),1)
+        self.assertEqual(deck.nextCard(),Card(3))
+        self.assertEqual(len(cards),0)
+    def testGenerate(self):
+        deck = Deck.generate()
+        cards = deck.getCards()
+        self.assertEqual(len(cards),44)
+        i = 0
+        for c in range(1,12):
+            for e in range(1,5):
+                self.assertEqual(cards[i].value,c)
+                i += 1
+
